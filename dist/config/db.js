056e7f1a8cf86f5f66a60d04f8273d13d558e364
Guillaume DELAMOTTE <guillaumedelamotte@macbookpro-1.home>
2026-02-24 09:40:01 +0100

    fix: use Prisma PG driver adapter for Prisma 7 compatibility

--- dist/config/db.js
+++ dist/config/db.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
+const adapter_pg_1 = require("@prisma/adapter-pg");
 require("dotenv/config");
-const prisma = new client_1.PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+const adapter = new adapter_pg_1.PrismaPg(connectionString);
+const prisma = new client_1.PrismaClient({ adapter });
 exports.default = prisma;
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaPg(connectionString);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
